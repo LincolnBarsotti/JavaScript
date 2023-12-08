580146d3f4769d7c8e843766774341918014070d
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2023-12-08 01:42:13 -0300

    Create type-number.js

---
+++ data structures/type-number.js
@@ -0,0 +1,22 @@
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+// Deve retornar 2
+console.log(operacaoMatematica);
+
+const numeroFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = pontoFlutuanteSemZero + numeroFlutuante;
+
+// Deve retornar 3.8
+console.log(novaOperacao);
+
+const naoEhUmNumero = "Nao é um numero";
+// Deve retornar NaN
+console.log(naoEhUmNumero * numeroFlutuante);
