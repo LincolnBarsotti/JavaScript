83984a43e9ed042696426e5ca946f655ff40ec19
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2023-12-08 10:52:54 -0300

    Add boolean structure

---
+++ data structures/type-boolean.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+// Deve retornar false
+console.log(primeiroNumero === segundoNumero);
+
+const resultado = primeiroNumero != segundoNumero;
+//Deve retornar true
+console.log(resultado);
+
+const falso = false
+
+console.log(falso);
