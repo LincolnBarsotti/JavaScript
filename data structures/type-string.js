68c8c92bf7068d8a0e6af442f9d6f7517822a1d7
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2023-12-08 01:42:55 -0300

    Add files via upload

---
+++ data structures/type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá mundo!";
+const texto2 = "Olá mundo!";
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'Lincoln enviou oi para ti';
+
+
+console.log(texto1);
+console.log(senha);
+console.log(StringDeNumeros);
+console.log(citacao);
+console.log(citacao + ' e ' + texto1);
+console.log('\u0024');
+// Template string ou template literal
