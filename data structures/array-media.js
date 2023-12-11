1eb71cb74ba8144c1c1da82f70a660d7fc943657
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2023-12-11 04:00:23 +0000

    Create array file

---
+++ data structures/array-media.js
@@ -0,0 +1,11 @@
+const nota1 = 10;
+const nota2 = 5.7;
+const nota3 = 8;
+const nota4 = 7.5;
+
+const notas = [10, 6.5, 7.6, 8];
+
+const media = (notas[0] + nota2 + nota3 + nota4)/notas.length;
+
+console.log(media);
+
