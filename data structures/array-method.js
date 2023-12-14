b52e4fd9560b118df8af60655d46b6b6a52a3218
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2023-12-14 02:43:08 +0000

    update file

--- data structures/array-method.js
+++ data structures/array-method.js
@@ -1,1 +1,18 @@
 const listaDeNumeros = [1,2,3,4,5,6];
+const lista2DeNumeros = [1,2,3,4,5,6];
+
+const numeros = listaDeNumeros.concat(lista2DeNumeros);
+
+console.log(numeros);
+
+// ------------------------------
+
+const num = [lista2DeNumeros, listaDeNumeros];
+
+console.log(num);
+
+// ----------------------------------
+
+const [first, second] = num;
+
+console.log(`Primeiro array: ${first} e o segundo array: ${second}`);
